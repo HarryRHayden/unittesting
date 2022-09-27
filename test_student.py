a74816eafd74d82e8921a73290f133fcc8ef3307
Harry Hayden <h_man1992@hotmail.com>
2022-09-27 12:03:16 +0000

    unittesting

--- test_student.py
+++ test_student.py
@@ -9,5 +9,18 @@ class TestStudent(unittest.TestCase):
         self.assertEqual(student.full_name, "John Doe")
 
 
+    def test_alert_santa(self):
+        student = Student("John", "Doe")
+        student.alert_santa()
+
+        self.assertTrue(student.naughty_list)
+
+
+    def test_email_address(self):
+        student = Student("John", "Doe")
+
+        self.assertEqual(student.email_address, "john.doe@email.com")
+
+
 if __name__ == '__main__':
     unittest.main()
