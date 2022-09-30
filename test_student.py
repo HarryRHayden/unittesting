5522c4c8fe2320b50407143010fe9cfb4374dd1b
Harry Hayden <h_man1992@hotmail.com>
2022-09-30 11:57:42 +0000

    add extension code

--- test_student.py
+++ test_student.py
@@ -1,5 +1,6 @@
 import unittest
 from student import Student
+from datetime import timedelta
 
 class TestStudent(unittest.TestCase):
 
@@ -38,6 +39,14 @@ class TestStudent(unittest.TestCase):
     def test_email_address(self):
         print('test_email_address')
         self.assertEqual(self.student.email_address, "john.doe@email.com")
+
+    
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+
 
 
 if __name__ == '__main__':
